4992f3d0c922acceb6f1f0e8db34cd492ef43a66
Prabin Shrestha <shresthaprabin315@gmail.com>
2022-01-14 18:46:41 +0545

    Created responsiveness for tablet and laptop for home page, changed website's title logo

---
+++ src/js/media.js
@@ -0,0 +1,13 @@
+window.onresize = window.onload = function () {
+  let width;
+  const searchBox = document.querySelector('.search__box');
+  const bookmarks = document.querySelector('.bookmarks');
+  width = this.innerWidth;
+  if (width >= 600) {
+    searchBox.placeholder = 'Search over 1,000,000 of books';
+    bookmarks.innerHTML = 'Bookmarks&nbsp;<i class="far fa-bookmark"></i>';
+  } else if (width < 600) {
+    searchBox.placeholder = 'Search Books';
+    bookmarks.innerHTML = '<i class="far fa-bookmark"></i>';
+  }
+};
