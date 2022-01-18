e9aefa3ea425a18c1f583cfbfece62c0b7836b78
Prabin Shrestha <shresthaprabin315@gmail.com>
2022-01-18 15:01:35 +0545

    Created Book Card Web Component, Added HTML structure for displaying book information, closing/opening book information, generated loading animation, styled components, book-info container

--- src/js/controller.js
+++ src/js/controller.js
@@ -4,31 +4,42 @@ import 'regenerator-runtime/runtime';
 
 (function () {
   /**
-   * *IIFE for Opening/Closing Bookmark Sidebar
+   * *IIFE for Opening/Closing Bookmark Sidebar and Book Information Section
    */
   const bookmarkBtn = document.querySelector('.bookmarks-btn');
   const bookmarks = document.querySelector('.bookmarks');
-  const bookmarkOverlay = document.querySelector('.bookmarks-overlay');
+  const overlay = document.querySelector('.overlay');
   const bookmarksClose = document.querySelector('.bookmarks-header span');
+  const bookInfo = document.querySelector('.book-info');
+  const bookInfoClose = document.querySelector('.book-info .close i');
 
   function addBookmark() {
     bookmarks.classList.add('active');
-    bookmarkOverlay.classList.add('active');
+    overlay.classList.add('active');
   }
   function removeBookmark() {
     bookmarks.classList.remove('active');
-    bookmarkOverlay.classList.remove('active');
+    setTimeout(() => overlay.classList.remove('active'), 100);
+  }
+  function removeBookInfo() {
+    bookInfo.classList.remove('active');
+    setTimeout(() => overlay.classList.remove('active'), 250);
   }
 
   // Bookmarks opens when clicked on bookmarks button
   bookmarkBtn.addEventListener('click', addBookmark);
 
-  //Bookmarks closes when clicked outside of bookmark sidebar, when clicked on close icon or when pressed Esc
-  bookmarkOverlay.addEventListener('click', removeBookmark);
+  //Bookmarks and Book Information closes when clicked outside of bookmark sidebar, when clicked on close icon or when pressed Esc
   bookmarksClose.addEventListener('click', removeBookmark);
+  bookInfoClose.addEventListener('click', removeBookInfo);
+  overlay.addEventListener('click', () => {
+    removeBookmark();
+    removeBookInfo();
+  });
   document.addEventListener('keydown', e => {
     if (e.key == 'Escape') {
       removeBookmark();
+      removeBookInfo();
     }
   });
 })();
@@ -151,3 +162,97 @@ const loadAuthorName = async function (array) {
   }
   return name;
 };
+
+// *BookItem Web Component
+const template = document.createElement('template');
+// slots can be used for adding information inside HTML Elements
+template.innerHTML = `
+<style>
+*{
+  margin: 0;
+  box-sizing: border-box;
+}
+.search-item {
+  display: flex;
+  flex-direction: row;
+  gap: 10px;
+}
+
+img {
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+h4 {
+  font-size: 16px;
+  color: #4b4b4b;
+  font-weight: 600;
+  padding-bottom: 4px;
+}
+
+h5 {
+  font-size: 12px;
+  color: #6c6c6c;
+  padding-bottom: 4px;
+}
+
+h6 {
+  font-size: 10px;
+  color: #6c6c6c;
+}
+</style>
+<div class="search-item" data-bookId="#">
+  <figure>
+    <img id='book-img'
+      width="70"
+      height="100"
+    />
+  </figure>
+  <div class="desc">
+    <h4><slot name='title'></h4>
+    <h5><slot name='author'></h5>
+    <h6><slot name='date'></h6>
+  </div>
+</div>
+`;
+
+class BookItem extends HTMLElement {
+  /**
+   * *Creating Custom HTML Element
+   */
+  constructor() {
+    super();
+
+    // *Attaching the current object to shadow DOM and appending new HTML templete to it's shadow root
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+    // *Setting image source, padding and justify-content style for this object component
+    this.shadowRoot.querySelector('img').src = this.getAttribute('url');
+    this.shadowRoot.querySelector('.search-item').style.paddingBottom =
+      this.getAttribute('padding');
+    this.shadowRoot.querySelector('.search-item').style.justifyContent =
+      this.getAttribute('justify-content');
+  }
+
+  connectedCallback() {
+    // *Event when user clicks to the image - It should show book information
+    this.shadowRoot.querySelector('img').addEventListener('click', () => {
+      const overlay = document.querySelector('.overlay');
+      const bookInfo = document.querySelector('.book-info');
+
+      bookInfo.classList.add('active');
+      overlay.classList.add('active');
+
+      // TODO:Render Book Information
+    });
+  }
+
+  disconnectedCallback() {
+    this.shadowRoot.querySelector('img').removeEventListener();
+  }
+}
+
+// *Adding the custom element to document
+customElements.get('book-item-component') ||
+  customElements.define('book-item-component', BookItem);
