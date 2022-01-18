b04b832b64a511581f464661d9e7cc27f03b19d4
Prabin Shrestha <shresthaprabin315@gmail.com>
2022-01-18 19:03:24 +0545

    Replaced old function that used to change search bar and bookmarks based on the width of the body with new resize observer

--- src/js/media.js
+++ src/js/media.js
@@ -1,13 +1,14 @@
-window.onresize = window.onload = function () {
-  let width;
+const bodyObserver = new ResizeObserver(entries => {
   const searchBox = document.querySelector('.search__box');
   const bookmarks = document.querySelector('.bookmarks-btn');
-  width = this.innerWidth;
-  if (width >= 600) {
+  const bodyObj = entries[0];
+  if (bodyObj.contentRect.width >= 600) {
     searchBox.placeholder = 'Search over 1,000,000 of books';
-    bookmarks.innerHTML = 'Bookmarks&nbsp;<i class="far fa-bookmark"></i>';
-  } else if (width < 600) {
+    bookmarks.innerHTML =
+      'Bookmarks&nbsp;&nbsp;<i class="far fa-bookmark"></i>';
+  } else if (bodyObj.contentRect.width < 600) {
     searchBox.placeholder = 'Search Books';
     bookmarks.innerHTML = '<i class="far fa-bookmark"></i>';
   }
-};
+});
+bodyObserver.observe(document.querySelector('body'));
